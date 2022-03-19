1c32acb78ba62a21496ca8b59068ce53e5e43024
Takumi Ikeda <tkik13201@gmail.com>
2022-03-20 01:10:44 +0900

    refactor: move styles to scss file

--- src/components/design/ScrollUp.tsx
+++ src/components/design/ScrollUp.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ScrollToTop from 'react-scroll-to-top';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import styles from 'assets/styles/components/design/ScrollUp.module.scss';
 
 const ScrollUp = (): JSX.Element => {
   return (
     <ScrollToTop
       smooth
-      style={{
-        borderRadius: '50%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
+      className={styles.root}
       component={<KeyboardArrowUpIcon />}
     />
   );
